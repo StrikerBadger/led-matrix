e812008e20f125cb3c1a745ad4151ccc7156446d
Aaron Heiniger <aheiniger@ethz.ch>
2026-06-13 12:39:31 +0200

    Function to load a pixel row

--- led_matrix_main_sketch/arduino_framework_basic_commands.ino
+++ led_matrix_main_sketch/arduino_framework_basic_commands.ino
@@ -1,3 +1,5 @@
+#include <math.h>
+
 void createInitialState() {
   // Make sure the output is off (the board is off is OE pin is high)
   digitalWrite(PIN_OE, HIGH);
@@ -17,3 +19,33 @@ void createInitialState() {
   digitalWrite(PIN_G2, LOW);
   digitalWrite(PIN_B2, LOW);
 }
+
+// Loads pixel array into the row(s)
+// Precondition: CLK is off (LOW)
+void loadPixelsIntoRow(int pixelsUpper[], int pixelsLower[]) {
+  int numPixelsUpper = sizeof(pixelsUpper)/(3*sizeof(pixelsUpper[0]));
+  int numPixelsLower = sizeof(pixelsLower)/(3*sizeof(pixelsLower[0]));
+
+  // Tests to warn about unexpected panel behaviour
+  if (numPixelsUpper > PANELWIDTH) {
+    printf("Warning: Pixel-Array passed is bigger than the row itself. Pixel-Array: %s\n", pixelsUpper);
+  }
+  if (numPixelsUpper != numPixelsLower) {
+    printf("Warning: Pixel-Array sizes don't match. Pixel-Arrays: %s, %s\n", pixelsUpper, pixelsLower);
+  }
+
+  for (int i = 0; i < max(numPixelsUpper, numPixelsLower); i++) {
+    // Set the pixel values
+    digitalWrite(PIN_R1, pixelsUpper[i*3]);
+    digitalWrite(PIN_G1, pixelsUpper[i*3+1]);
+    digitalWrite(PIN_B1, pixelsUpper[i*3+2]);
+    digitalWrite(PIN_R2, pixelsLower[i*3]);
+    digitalWrite(PIN_G2, pixelsLower[i*3+1]);
+    digitalWrite(PIN_B2, pixelsLower[i*3+2]);
+    // Advance by clocking
+    digitalWrite(PIN_CLK, HIGH);
+    digitalWrite(PIN_CLK, LOW);
+  }
+}
+// Postcondition: Pixels are loaded, CLK is off (LOW)
+
--- led_matrix_main_sketch/led_matrix_main_sketch.ino
+++ led_matrix_main_sketch/led_matrix_main_sketch.ino
@@ -18,9 +18,19 @@
 #define PIN_D 5
 #define PIN_E 2  
 
-void setup() {
-  // put your setup code here, to run once:
+#define PANEL_HEIGHT 64
+#define PANEL_WIDTH 128
+#define NUM_SCANLINES 2
 
+void setup() {
+  // Set the pins into their respective modes
+  pinMode(PIN_R1, OUTPUT); pinMode(PIN_G1, OUTPUT); pinMode(PIN_B1, OUTPUT);
+  pinMode(PIN_R2, OUTPUT); pinMode(PIN_G2, OUTPUT); pinMode(PIN_B2, OUTPUT);
+  pinMode(PIN_CLK, OUTPUT); pinMode(PIN_LAT, OUTPUT); pinMode(PIN_OE, OUTPUT);
+  
+  pinMode(PIN_A, OUTPUT); pinMode(PIN_B, OUTPUT); 
+  pinMode(PIN_C, OUTPUT); pinMode(PIN_D, OUTPUT);
+  pinMode(PIN_E, OUTPUT);
 }
 
 void loop() {
